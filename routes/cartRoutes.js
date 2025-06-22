c071412c39b6d69cd08bb0b2a36200e888843047
NandiniKitike2001 <Nandini@taxiondc.com>
2025-06-22 14:52:26 +0530

    added

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,15 +3,11 @@ const cartController = require("../controllers/cartController");
 const router = express.Router();
 const authMiddleware = require("../Middleware/authMiddleware");
 
-router.post("/add", authMiddleware, cartController.addToCart);
-router.get("/getcart", authMiddleware, cartController.getCart);
-router.put("/update", authMiddleware, cartController.updateCartItem);
-router.delete(
-  "/remove/:productId",
-  authMiddleware,
-  cartController.removeCartItem
-);
-router.delete("/clear", authMiddleware, cartController.clearCart);
-router.get("/count", authMiddleware, cartController.getCartItemCount);
+router.post("/add", cartController.addToCart);
+router.get("/getcart", cartController.getCart);
+router.put("/update", cartController.updateCartItem);
+router.delete("/remove/:productId", cartController.removeCartItem);
+router.delete("/clear", cartController.clearCart);
+router.get("/count", cartController.getCartItemCount);
 
 module.exports = router;
