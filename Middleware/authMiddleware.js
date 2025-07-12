daf7a06a2a384eab39586f678349ef58f2c3a8c5
NandiniKitike2001 <Nandini@taxiondc.com>
2025-07-12 20:04:39 +0530

    dev backend

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,21 +1,15 @@
 const jwt = require("jsonwebtoken");
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const app = express();
+
 const authMiddleware = (req, res, next) => {
   const token = req.cookies?.token;
 
   console.log(token, "token");
-  app.use(cookieParser());
+
   if (!token) {
     return res
       .status(401)
       .json({ message: "Access denied. No token provided." });
   }
-  // if (!authHeader || !authHeader.startsWith("Bearer ")) {
-  //   console.log("No token or invalid format:", authHeader);
-  //   return res.status(401).json({ message: "Access denied" });
-  // }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
