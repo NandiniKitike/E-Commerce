3545f87ad5040edf3d64d0629e1dcf7ca3ad142b
NandiniKitike2001 <Nandini@taxiondc.com>
2025-07-12 19:44:53 +0530

    login screen doone

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,39 +1,10 @@
-// const jwt = require("jsonwebtoken");
-
-// const authMiddleware = (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-
-//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//     return res.status(401).json({
-//       message: "Access denied. No token provided or invalid format.",
-//     });
-//   }
-
-//   const token = authHeader.split(" ")[1];
-
-//   try {
-//     const decoded = jwt.verify(token, "nandini"); // Same secret used here
-
-//     req.user = {
-//       id: decoded.id,
-//       role: decoded.role,
-//     };
-
-//     next();
-//   } catch (err) {
-//     console.error("Token verification error:", err.message);
-//     return res.status(401).json({ message: "Invalid token" });
-//   }
-// };
-
-// module.exports = authMiddleware;
 const jwt = require("jsonwebtoken");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token; // ✅ Get token from cookie
-  const express = require("express");
-  const cookieParser = require("cookie-parser");
+  const token = req.cookies?.token;
 
-  const app = express();
   console.log(token, "token");
   app.use(cookieParser());
   if (!token) {
